58d994327f694dcf2acfd098c0aefb6d6cd9d03c
Ankita Yadav <ankitayaduvanshi226@gmail.com>
2023-11-27 12:39:07 +0530

    soving operators questions

--- Operators.java
+++ Operators.java
@@ -1,5 +1,41 @@
 public class Operators {
     public static void main(String args[]){
-System.out.println("hello");
+  // ** increment operator
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+        //  int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+    // ** decrement operator
+    //    int a = 10;
+    //    int b =--a;
+    //    System.out.println(a);   
+    //    System.out.println(b);   
+    //    int a = 10;
+    //    int b =a--;
+    //    System.out.println(a);   
+    //    System.out.println(b);   
+//** Relational Operators
+// int A = 10;
+// int B = 9;
+// System.out.println(B>A);
+
+//** Logical Operator */
+//Logical AND - &&
+// Logical OR - ||
+// Logical NOT - !
+
+// System.out.println((3>2) && (5<0));
+// System.out.println((3>2) || (5<0));
+// System.out.println(!(3>2));
+// ** Assignment operator 
+// int C = 10;
+// C += 10;
+// System.out.println(C);
+
+
     }
-}
+    }
